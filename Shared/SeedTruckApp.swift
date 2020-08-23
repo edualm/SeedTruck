f120c9bbc78871f4a97d838fd05931461f15f358
Eduardo Almeida <me@edr.io>
2020-08-23 02:39:55 +0100

    Initial Commit

---
+++ Shared/SeedTruckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedTruckApp.swift
+//  Shared
+//
+//  Created by Eduardo Almeida on 23/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SeedTruckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
