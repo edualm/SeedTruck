6f18086b69db1008e8d7bd0c02bfe6ff8b568413
Eduardo Almeida <me@edr.io>
2020-08-25 11:35:40 +0100

    Started work on torrent parsing.

--- Shared/Models/LocalTorrent.swift
+++ Shared/Models/LocalTorrent.swift
@@ -5,6 +5,7 @@
 //  Created by Eduardo Almeida on 23/08/2020.
 //
 
+import Bencode
 import Foundation
 
 enum LocalTorrent {
@@ -21,9 +22,11 @@ extension LocalTorrent {
             return magnet.slice(from: "dn=", to: "&")?.replacingOccurrences(of: "+", with: " ")
             
         case .torrent(let torrent):
-            ()
+            guard let decoded = try? Bencoder().decode(bytes: torrent.map { $0 }) else {
+                return nil
+            }
+            
+            return decoded["info"]["name"].string
         }
-        
-        return nil
     }
 }
